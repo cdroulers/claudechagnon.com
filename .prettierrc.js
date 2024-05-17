99a5f1e8bec0e86600293aeb0661f85a455ac085
Christian Droulers <droulers.christian@gmail.com>
2024-05-16 22:55:23 -0400

    Update templates, add test

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-    bracketSpacing: true,
-    endOfLine: "lf",
-    bracketSameLine: true,
-    printWidth: 100,
-    semi: true,
-    singleQuote: false,
-    tabWidth: 2,
-    trailingComma: "es5",
-  };
-  
+  bracketSpacing: true,
+  endOfLine: "lf",
+  bracketSameLine: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
